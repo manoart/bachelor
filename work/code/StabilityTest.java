6a4e35f1bee7d6a6f9ce507b97ade1387e69f667
Manuel Schwarz <manschwa@uos.de>
2012-03-26 17:40:55 +0200

    wrote Einleitung und Schluss

--- work/code/StabilityTest.java
+++ work/code/StabilityTest.java
@@ -1,6 +1,5 @@
 private int stabilityTest(Voxel v)
 {
-    int avalanche = 0;
     // test all 8 neighbors and calculate their height difference
     if(v.hasRightNeighbor())
     {
@@ -8,7 +7,6 @@ private int stabilityTest(Voxel v)
         {
             avalanche(v.getRightNeighbor()).raiseDensity(SNOWFLAKE / 6.0f);
             stabilityTest(avalanche(v.getRightNeighbor()));
-            avalanche++;
         }
     }
     ...
