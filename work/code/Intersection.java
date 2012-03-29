97c635d5c78c28394d05ed86db709b79a82fef9e
Manuel Schwarz <manschwa@uos.de>
2012-03-29 10:15:10 +0200

    work ready for print

--- work/code/Intersection.java
+++ work/code/Intersection.java
@@ -6,10 +6,12 @@ for(int iEdge = 0; iEdge < 12; iEdge++)
     {
         edgeVertex[iEdge] =
             new Vertex((v.getX() + (VOXEL_OFFSET[EDGE_CONNECTION[iEdge][0] ][0]+
-                           0.5f * EDGE_DIRECTION[iEdge][0]) * scale),
+                       interpolate(v.getDensity(), v.getNeighbor().getDensity())
+                        * EDGE_DIRECTION[iEdge][0]) * scale),
                        (v.getY() + (VOXEL_OFFSET[EDGE_CONNECTION[iEdge][0] ][1]+
-                           0.5f * EDGE_DIRECTION[iEdge][1]) * scale),
+                       interpolate(v.getDensity(), v.getNeighbor().getDensity())
+                        * EDGE_DIRECTION[iEdge][1]) * scale),
                        (v.getZ() + (VOXEL_OFFSET[EDGE_CONNECTION[iEdge][0] ][2]+
-                           v.getDensity() * EDGE_DIRECTION[iEdge][2]) * scale));
+                        v.getDensity() * EDGE_DIRECTION[iEdge][2]) * scale));
     }
 }
